dd84f3c9b5b50e9516e8fe830c536a1756ec0498
V4L3NT1N3 <nicholaschn@protonmail.com>
2021-06-20 17:03:25 +0800

    Removed existing widgets

--- lib/main.dart
+++ lib/main.dart
@@ -15,74 +15,35 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  // variables holding quote and author data, set to loading... at first
-
-  String quote = 'loading ...';
-  String author = 'loading ...';
-
-  // get quote API data, store the data above
-
-  void getQuote () async {
-    QuoteAPI newQuote = QuoteAPI();
-    Map data = await newQuote.getData();
-    setState(() {
-      quote = data["content"];
-      author = data["author"];
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getQuote();
-  }
+  // // variables holding quote and author data, set to loading... at first
+  //
+  // String quote = 'loading ...';
+  // String author = 'loading ...';
+  //
+  // // get quote API data, store the data above
+  //
+  // void getQuote () async {
+  //   QuoteAPI newQuote = QuoteAPI();
+  //   Map data = await newQuote.getData();
+  //   setState(() {
+  //     quote = data["content"];
+  //     author = data["author"];
+  //   });
+  // }
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getQuote();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.drive_file_rename_outline, size: 30, color: Colors.black54),
-          centerTitle: true,
-          title: Text('Minimal Quotes', style: TextStyle(color: Colors.black54)),
-          backgroundColor: Colors.grey[50],
-          elevation: 0,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(quote,
-                  style: TextStyle(fontSize: 30)),
-              SizedBox(height: 50),
-              Text(author,
-                  style: TextStyle(fontSize: 20, color: Colors.grey)),
-              SizedBox(height: 120),
-              Center(
-                child: FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      getQuote();
-                    });
-                  },
-                  color: Colors.black87,
-                  minWidth: 20,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Next Quote', style: TextStyle(color: Colors.grey[50])),
-                      SizedBox(width: 10),
-                      Icon(Icons.arrow_forward_rounded, color: Colors.grey[50]),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+
+        // add your widgets here
+
       ),
     );
   }
